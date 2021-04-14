ac5b551d6329820bde2c361b71abf48cd821b64f
AndreiApostu <61390325+AndreiApostu@users.noreply.github.com>
2021-04-14 18:02:36 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 startTime = time.time() #Registers when the code started running
 
 client = commands.Bot(command_prefix= "?")
+discord.Intents.all()
 
 @client.event
 async def on_connect():
@@ -17,5 +18,12 @@ async def on_ready():
 @client.event
 async def on_disconnect():
     print(f'Lost connection to Discord | Time elapsed: {str(round(time.time() - startTime, 2))} seconds.')
+
+@client.event
+async def on_member_join(member):
+    print("A")
+    if member.guild.system_channel is not None:
+        print("B")
+        await member.guild.system_channel().send(f'{member.mention()} has joined {member.guild}')
 
 client.run("ODMxMzc1OTAwNjE2NDI1NDky.YHUVJA.IKQJdRn5b2I7Spu3Ozei3glkAC4")
