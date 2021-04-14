97e7a6dfdc6acdc4dda9c551591a33bdbad5e522
AndreiApostu <61390325+AndreiApostu@users.noreply.github.com>
2021-04-14 18:13:50 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 startTime = time.time() #Registers when the code started running
 
 client = commands.Bot(command_prefix= "?")
-discord.Intents.all()
+#client = discord.Intents.all()
 
 @client.event
 async def on_connect():
@@ -26,4 +26,12 @@ async def on_member_join(member):
         print("B")
         await member.guild.system_channel().send(f'{member.mention()} has joined {member.guild}')
 
+@client.event
+async def on_member_remove(member):
+    print("A")
+    if member.guild.system_channel is not None:
+        print("B")
+        await member.guild.system_channel().send(f'{member.mention()} has left {member.guild}')
+
+client.Intents = discord.Intents.all()
 client.run("ODMxMzc1OTAwNjE2NDI1NDky.YHUVJA.IKQJdRn5b2I7Spu3Ozei3glkAC4")
