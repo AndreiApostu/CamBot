ae71a23c50dbcbd78015855fe2233ae44a21d93f
AndreiApostu <61390325+AndreiApostu@users.noreply.github.com>
2021-04-14 07:27:41 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,17 @@
+import time 
 import discord
 from discord.ext import commands
+
+startTime = time.time()
 
 client = commands.Bot(command_prefix= "?")
 
 @client.event
+async def on_connect():
+    print(f'Successfully connected to Discord | Time elapsed: {str(round(time.time() - startTime, 2))} seconds.')
+
+@client.event
 async def on_ready():
-    print("Online")
+    print(f'Logged in as {client.user} | Time elapsed: {str(round(time.time() - startTime, 2))} seconds.\nLatency: { str(round(client.latency * 1000))} milliseconds')
 
 client.run("ODMxMzc1OTAwNjE2NDI1NDky.YHUVJA.IKQJdRn5b2I7Spu3Ozei3glkAC4")
