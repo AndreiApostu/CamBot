8076b0b666d7165da68d6dfce4c993b60eddda59
AndreiApostu <61390325+AndreiApostu@users.noreply.github.com>
2021-04-14 04:51:22 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,18 @@ client = commands.Bot(command_prefix= "?")
 
 @client.event
 async def on_ready():
-    print("Online")
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+    elif message.content.startswith("shutdown"):
+        await message.channel.send("Terminating bot...")
+        print("Bot terminated")
+        exit()
 
 client.run("ODMxMzc1OTAwNjE2NDI1NDky.YHUVJA.IKQJdRn5b2I7Spu3Ozei3glkAC4")
