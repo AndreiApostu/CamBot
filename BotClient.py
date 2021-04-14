e3c5dc25480f0b53914f863c9ae20c00c86723e3
AndreiApostu <61390325+AndreiApostu@users.noreply.github.com>
2021-04-14 21:05:17 +0300

    Update BotClient.py

--- BotClient.py
+++ BotClient.py
@@ -18,13 +18,9 @@ class BotClient(discord.Client):
         print(f'Lost connection to Discord | Time elapsed: {str(round(time.time() - self.startTime, 2))} seconds.')
 
     async def on_member_join(self, member):
-        print("A")
         if member.guild.system_channel is not None:
-            print("B")
             await member.guild.system_channel.send(f'{member.mention} has joined {member.guild}')
 
     async def on_member_remove(self, member):
-        print("A")
         if member.guild.system_channel is not None:
-            print("B")
             await member.guild.system_channel.send(f'{member.mention} has left {member.guild}')
