e312b7040a758915c1f062a7964fd44cb118e1c3
AndreiApostu <61390325+AndreiApostu@users.noreply.github.com>
2021-06-30 04:48:36 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,29 +1,52 @@
-import time
+import time 
 import discord
+import sys
+import logging
+
 from discord.ext import commands
 
-startTime = time.time()
+logging.basicConfig(level=logging.INFO)
 
-client = commands.Bot(command_prefix= "?")
+startTime = time.time()
+#client = discord.Client(intents=discord.Intents.all())
+client = commands.Bot(command_prefix= "?",intents = discord.Intents.all(), case_insensitive = True)
 
 @client.event
 async def on_connect():
-    print(f'Successfully connected to Discord | Time elapsed: {str(round(time.time() - startTime, 2))} seconds.')
+     print(f'Successfully connected to Discord | Time elapsed: {str(round(time.time() - startTime, 2))} seconds.')
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} | Time elapsed: {str(round(time.time() - startTime, 2))} seconds.\nLatency: { str(round(client.latency * 1000))} milliseconds')
-#print('Logged in as {0.user}'.format(client))
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    await client.change_presence(activity=discord.Game('Everything i put effort into turns to shit'))
 
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
-    elif message.content.startswith("shutdown"):
-        await message.channel.send("Terminating bot...")
-        print("Bot terminated")
-        exit()
+@client.event
+async def on_disconnect():
+    print(f'Lost connection to Discord | Time elapsed: {str(round(time.time() - startTime, 2))} seconds.')
+
+@client.event
+async def on_member_join(member):
+    if member.guild.system_channel is not None:
+        await member.guild.system_channel.send(f'{member.mention} has joined {member.guild}')
+
+@client.event
+async def on_member_remove(member):
+    if member.guild.system_channel is not None:
+         await member.guild.system_channel.send(f'{member.mention} has left {member.guild}')
+
+@client.command()
+async def Avatar(ctx, member: discord.Member = None):
+    if member is None:
+        member = ctx.author
+    await ctx.send(f'{member.avatar_url}')
+
+@client.command()
+async def Latency(ctx):
+    await ctx.send(f'{str(round(client.latency * 1000))}ms')
+
+@client.command()
+async def Shut(ctx):
+    await ctx.send('Shutting Down...')
+    sys.exit()
 
 client.run("ODMxMzc1OTAwNjE2NDI1NDky.YHUVJA.IKQJdRn5b2I7Spu3Ozei3glkAC4")
